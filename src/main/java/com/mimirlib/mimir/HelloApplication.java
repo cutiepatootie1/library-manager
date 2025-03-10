fc5ba584cd0ad0a5db5c3b28347a598babff028f
Nero <amacaneil@gmail.com>
2025-03-11 01:48:38 +0800

    made 0.001 progress whapjfasd gonna sleep... maybe

--- src/main/java/com/mimirlib/mimir/HelloApplication.java
+++ src/main/java/com/mimirlib/mimir/HelloApplication.java
@@ -6,6 +6,8 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -16,10 +18,21 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         Image icon = new Image("file:src/asset/icon.png");
 
+        //group node shit
         Group root = new Group();
-        Scene scene = new Scene(root, Color.rgb(40, 40, 40));
+        //scene customizations
+        Scene scene = new Scene(root, 900, 620, Color.rgb(40, 40, 40));
         stage.getIcons().add(icon);
         stage.setTitle("Mimir - the library manager");
+
+        Text welcome = new Text();
+        welcome.setText("A helpful assistant software that manages all your books");
+        welcome.setX(50);
+        welcome.setY(50);
+        welcome.setFont(Font.font("Consolas", 25));
+        welcome.setFill(Color.WHITE);
+
+        root.getChildren().add(welcome);
         stage.setScene(scene);
         stage.show();
     }
